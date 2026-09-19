4d589e233dc6b1df566e501c7b39ba46c71198e6
lsands5400 <lsands@g.hmc.edu>
2026-09-18 22:59:59 -0400

    lab 3 init

---
+++ lab3_ls.sv
@@ -0,0 +1,9 @@
+// Lindsey Sands
+// lsands@g.hmc.edu
+// 9-18-2026
+// This is the top level module for Lab 3. It takes button press inputs from a keypad
+// and sends corresponding hex digit outputs to a multiplexed seven segment display. 
+module lab3_ls();
+
+    
+endmodule
