d3692866645283b3b1356853b04c7fde2b10d0dd
lsands5400 <lsands@g.hmc.edu>
2026-09-19 23:23:08 -0400

    preliminary buttonPress module

--- buttonPress.sv
+++ buttonPress.sv
@@ -12,7 +12,6 @@ module buttonPress(input logic clk, reset, press,
     always_ff @(posedge clk) begin
         if (!reset)          state <= S0;
         else                state <= nextstate;
-
     end
 
     always_comb
@@ -22,20 +21,41 @@ module buttonPress(input logic clk, reset, press,
                 // The system is ready to register a new button press if:
                 // 1. No other buttons are being pressed
                 // 2. Debouncing is not occurring
-                S0: 
+                S0: if (press == 0) begin             
+                        nextstate <= S0;
+                        enSSDP <= 0;
+                    end
+                    else begin
+                        nextstate <= S1;
+                        enSSDP <= 1;
+                    end
 
                 // State 1: 
                 // Initial press is registered and hex value is sent to seven 
                 // segment display
-                S1:
+                S1: if (press == 0) begin             
+                        nextstate <= S0;
+                        enSSDP <= 0;
+                    end
+                    else begin
+                        nextstate <= S2;
+                        enSSDP <= 0;
+                    end
 
                 // State 2:
                 // The system is not ready to register a new button press if:
                 // 1. Another button is currently being pressed
                 // 2. Debouncing is occuring
-                S2:
+                S2: if (press == 0) begin             
+                        nextstate <= S0;
+                        enable <=0;
+                    end
+                    else begin
+                        nextstate <= S2;
+                        enSSDP <= 0;
+                    end
 
-                default:
+                default: S0;
             endcase
         end
 
