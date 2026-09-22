10e6829a23d580d85a183d5ec2a80f37193be66c
lsands5400 <lsands@g.hmc.edu>
2026-09-22 15:23:51 -0700

    keypad decoder testbench

---
+++ testbenches/decodeKeypad_tb.sv
@@ -0,0 +1,42 @@
+// Lindsey Sands
+// lsands@g.hmc.edu
+// 09-18-2026
+// This is the testbench module for the keypad decoder module.
+`timescale 1 ns/1 ns
+
+module decodeKeypad_tb();
+
+	logic[3:0]	col, row, hex;
+    
+    decodeKeypad dut (
+		.col(col),
+		.row(row),
+
+        .hex(hex)
+    );
+	
+    always begin
+	clk = 0; #5;
+    clk = 1; #5;
+    end
+
+    initial begin
+
+    row = 4'b0010;
+	col = 4'b0001;
+	#10;                        // wait required time
+	assert (hex == 4'b0100)       // check outputs
+	else 
+		$error("Keypad Decoder test 1 failed."); 
+	
+    #10; 
+    row = 4'b1000;
+	col = 4'b1000;
+	#10;                        // wait required time
+	assert (hex == 4'b1101)       // check outputs
+	else 
+		$error("Keypad Decoder test 2 failed."); 
+	
+    #100 $stop;
+    end
+endmodule
